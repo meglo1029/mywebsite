93de84c3911b81ba8c17d3d1950885b74a4d8c60
Meghan Logan <meglo1029@gmail.com>
2017-07-08 17:11:10 -0700

    adding new pages, making them responsive, work in progress

--- main.js
+++ main.js
@@ -54,6 +54,25 @@ $(".mynametext").click(function(){
 });
 
 
+// ADD DROPDOWN MENU -----
+
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
 
 
 
